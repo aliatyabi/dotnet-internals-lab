2aeba4ff9605d6a2b8ffe0aa44c19190bcd9ca22
ali atyabi <a.atyabi@rayanbourse.ir>
2025-12-27 14:28:11 +0330

    add event-based memory leak example (bad version)

---
+++ src/04-memory-leaks/MemoryLeaks.Lab/EventLeak/Subscriber.cs
@@ -0,0 +1,19 @@
+﻿namespace MemoryLeaks.Lab.EventLeak
+{
+    public class Subscriber
+    {
+        private readonly int _id;
+
+        public Subscriber(Publisher publisher, int id)
+        {
+            _id = id;
+            publisher.OnMessage += Handle;
+        }
+
+        private void Handle()
+        {
+            Console.WriteLine($"Subscriber {_id} received message");
+        }
+    }
+
+}
---
+++ src/04-memory-leaks/MemoryLeaks.Lab/Program.Bad.cs
@@ -0,0 +1,26 @@
+﻿using MemoryLeaks.Lab.EventLeak;
+
+class ProgramBad
+{
+    static Publisher publisher = new Publisher();
+
+    static void Main()
+    {
+        CreateSubscribers();
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
+
+        Console.WriteLine("GC forced. Subscribers should be gone but they don't.");
+        Console.ReadLine();
+    }
+
+    static void CreateSubscribers()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            new Subscriber(publisher, i);
+        }
+    }
+}
